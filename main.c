eefbfa81760836e27b4a83870ff0d2c02b2fa21d
liht0 <liht0yager@gmail.com>
2025-05-28 21:35:31 +1000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <string.h>
+
+sigjmp_buf jmpbuf;
+
+void signal_handler(int sig) {
+    const char *sig_name;
+    
+    switch(sig) {
+        case SIGABRT: sig_name = "SIGABRT"; break;
+        case SIGFPE:  sig_name = "SIGFPE";  break;
+        case SIGILL:  sig_name = "SIGILL";  break;
+        case SIGINT:  sig_name = "SIGINT";  break;
+        case SIGSEGV: sig_name = "SIGSEGV"; break;
+        case SIGTERM: sig_name = "SIGTERM"; break;
+        default:      sig_name = "UNKNOWN"; break;
+    }
+    
+    printf("\poluchen signal: %s (%d)\n", sig_name, sig);
+    
+    if (sig == SIGSEGV) {
+        siglongjmp(jmpbuf, 1);
+    }
+}
+
+
+void generate_segfault() {
+    int *ptr = NULL;
+    *ptr = 42; 
+}
+
+int main() {
+    signal(SIGABRT, signal_handler);
+    signal(SIGFPE,  signal_handler);
+    signal(SIGILL,  signal_handler);
+    signal(SIGINT,  signal_handler);
+    signal(SIGSEGV, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    if (sigsetjmp(jmpbuf, 1) == 0) {
+        printf("obrabotka zapushena\n");
+        printf("dostupnie signali:\n");
+        printf("1. SIGABRT  2. SIGFPE  3. SIGILL\n");
+        printf("4. SIGINT   5. SIGSEGV 6. SIGTERM\n");
+        printf("0. Exit\n");
+
+        int choice;
+        while(1) {
+            printf("\nVibeirte (0-6): ");
+            scanf("%d", &choice);
+            
+            switch(choice) {
+                case 0:
+                    printf("program\n");
+                    exit(0);
+                
+                case 1:
+                    printf("Генерация SIGABRT\n");
+                    abort();
+                    break;
+                
+                case 2:
+                    printf("Генерация SIGFPE\n");
+                    int a = 10, b = 0;
+                    int c = a / b; // Деление на ноль
+                    break;
+                
+                case 3:
+                    printf("Генерация SIGILL\n");
+                    raise(SIGILL);
+                    break;
+                
+                case 4:
+                    printf("Генерация SIGINT\n");
+                    raise(SIGINT);
+                    break;
+                
+                case 5:
+                    printf("Генерация SIGSEGV\n");
+                    generate_segfault();
+                    break;
+                
+                case 6:
+                    printf("Генерация SIGTERM\n");
+                    raise(SIGTERM);
+                    break;
+                
+                default:
+                    printf("Неверный выбор!\n");
+            }
+        }
+    }
+
+    return 0;
+}
